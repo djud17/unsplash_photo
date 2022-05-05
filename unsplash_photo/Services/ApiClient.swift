e595e7098550d3297aa254810bd32fb84c06b131
David Tonoyan <davidtonoyan10@me.com>
2022-05-05 13:21:11 +0300

    ApiClient Finished

--- unsplash_photo/Services/ApiClient.swift
+++ unsplash_photo/Services/ApiClient.swift
@@ -14,13 +14,21 @@ enum ApiError: Error {
 
 protocol ApiClient {
     func getRandomPhoto(completion: @escaping (Result<[Photo], Error>) -> Void)
+    func getSearchPhoto(_ searchWord: String, completion: @escaping (Result<SearchPhoto, Error>) -> Void)
 }
 
 class ApiClientImpl: ApiClient {
-    private let unsplashUrl = "https://api.unsplash.com/photos/"
+    // Parameters for request
+    private let unsplashUrl = "https://api.unsplash.com/"
     private let accessKey = "?client_id=9etn9yyFeBTPlHH66IhXTisehts2b5rGd8Tok48XLK8"
-    private let randomPhotoUrl = "random/"
+    
+    // Parameters for random photo request
+    private let randomPhotoUrl = "photos/random/"
     private let photoCounter = "&count=10"
+    
+    // Parameters for search photo request
+    private let searchPhotoUrl = "search/photos/"
+    private let searchRequest = "&query=fruits"
     
     func getRandomPhoto(completion: @escaping (Result<[Photo], Error>) -> Void) {
         let apiUrl = URL(string: "\(unsplashUrl)\(randomPhotoUrl)\(accessKey)\(photoCounter)")!
@@ -30,6 +38,24 @@ class ApiClientImpl: ApiClient {
                 let randomPhotos: [Photo] = try! JSONDecoder().decode([Photo].self, from: data)
                 if response.response?.statusCode == 200 {
                     completion(.success(randomPhotos))
+                } else {
+                    completion(.failure(ApiError.wrongData))
+                }
+            } else {
+                completion(.failure(ApiError.noData))
+            }
+        }
+    }
+
+    func getSearchPhoto(_ searchWord: String, completion: @escaping (Result<SearchPhoto, Error>) -> Void) {
+        let strUrl = "\(unsplashUrl)\(searchPhotoUrl)\(accessKey)\(searchRequest)\(searchWord)".addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+        let apiUrl = URL(string: strUrl)!
+        
+        AF.request(apiUrl).responseData { response in
+            if let data = response.value {
+                let searchPhotos: SearchPhoto = try! JSONDecoder().decode(SearchPhoto.self, from: data)
+                if response.response?.statusCode == 200 {
+                    completion(.success(searchPhotos))
                 } else {
                     completion(.failure(ApiError.wrongData))
                 }
